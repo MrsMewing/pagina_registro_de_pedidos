aa3fba36a0b4cc230bf3758b98ddf989c7e278c6
DiegoDev.py <163680936+MrsMewing@users.noreply.github.com>
2025-09-27 14:07:23 -0600

    Update registro_services_workers.js
    Se quito el codigo que estaba en el script que registra el service worker

--- registro_services_workers.js
+++ registro_services_workers.js
@@ -1,12 +1,1 @@
-//verifica si el navegador soporta services workers y agrega el service worker
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/pagina_registro_de_pedidos/firebase-messaging-sw.js')
-  .then(reg => {
-    console.log('Service Worker registrado:', reg);
-  })
-  .catch(err => {
-    console.error('Error al registrar el SW:', err);
-  });
-
-}
