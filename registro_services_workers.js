b20c82f46ad91d77ee7a06bfae661d451b00c74c
DiegoDev.py <163680936+MrsMewing@users.noreply.github.com>
2025-09-27 13:34:26 -0600

    Update registro_services_workers.js desde repositorio en la nube
    actualizacion en la ruta , ahora es /pagina_registro_de_pedidos/firebase-messaging-sw.js

--- registro_services_workers.js
+++ registro_services_workers.js
@@ -1,7 +1,7 @@
 //verifica si el navegador soporta services workers y agrega el service worker
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('notificaciones-sw.js')
+  navigator.serviceWorker.register('/pagina_registro_de_pedidos/firebase-messaging-sw.js')
   .then(reg => {
     console.log('Service Worker registrado:', reg);
   })
